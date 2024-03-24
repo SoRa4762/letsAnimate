d1d6b41ed83a60d0415439fe320963e75ce32234
SoRa <sonamraisx007@gmail.com>
2024-03-24 20:57:37 +0545

    I only need to figure a way out to add and remove simulateously mate

--- src/Animations/SignInXSignUp.jsx
+++ src/Animations/SignInXSignUp.jsx
@@ -1,33 +1,81 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SignInXSignUp = () => {
   const [isSignIn, setIsSignIn] = useState(true);
   const handleSwitch = () => {
     setIsSignIn(!isSignIn);
-    console.log(isSignIn);
+    // console.log(isSignIn);
   };
 
   return (
     <>
-      <div className="h-[100vh] w-full p-8 sm:p-12 md:p-16 lg:p-20">
-        {/* sign in */}
-        <div
-          className={`${
-            isSignIn ? "flex" : "hidden"
-          } h-full w-full bg-blue-600`}
-        >
-          <div></div>
-          <div></div>
+      <div className="h-[100vh] w-full p-8 sm:p-12 md:p-16 lg:p-20 bg-slate-200">
+        {/* sign in/sign up*/}
+        <div className="h-full w-full flex bg-white rounded-[2rem] shadow-2xl">
+          {/* first half */}
+          <div
+            className={`${
+              isSignIn
+                ? "transform translate-x-0 rounded-r-[11rem] rounded-l-3xl"
+                : "transform translate-x-[100%] rounded-l-[11rem] rounded-r-3xl"
+            } h-full flex-1 bg-yellow-500 duration-700`}
+          >
+            {isSignIn ? (
+              <div className="h-full w-full flex justify-center items-center">
+                <button
+                  className={`h-12 w-16 bg-red-600`}
+                  onClick={handleSwitch}
+                >
+                  Sign In
+                </button>
+              </div>
+            ) : (
+              <div className="h-full w-full flex justify-center items-center">
+                <button
+                  className={`h-12 w-16 bg-blue-600`}
+                  onClick={handleSwitch}
+                >
+                  Sign Up
+                </button>
+              </div>
+            )}
+          </div>
+
+          {/* second half */}
+          <div
+            className={`${
+              isSignIn
+                ? "transform translate-x-0"
+                : "transform translate-x-[-100%]"
+            } animate-none flex-1 duration-700 bg-red-300`}
+          >
+            {useEffect(() => {
+              isSignIn ? (
+                <div className="animate-fade duration-700">
+                  <h1>Sign In Mate!</h1>
+                </div>
+              ) : (
+                <div className="animate-fade duration-700">
+                  <h1>Sign In Mate!</h1>
+                </div>
+              );
+            }, [isSignIn])}
+          </div>
         </div>
 
-        <div className="h-24 w-full flex">
-          <button className="h-full w-full bg-red-600" onClick={handleSwitch}>
+        {/* <div className="h-24 w-full flex">
+          <button
+            className={`${
+              !isSignIn && "disabled:bg-red-900"
+            } h-full w-full bg-red-600`}
+            onClick={handleSwitch}
+          >
             Sign In
           </button>
           <button className="h-full w-full bg-blue-600" onClick={handleSwitch}>
             Sign Up
           </button>
-        </div>
+        </div> */}
       </div>
     </>
   );
